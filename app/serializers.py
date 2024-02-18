1ab04442fa6b36e6c75e5b9f84941b5b3a521e0e
neerajpandit <pandeyneeraj1507@gmail.com>
2024-02-18 18:30:09 +0530

    ini

---
+++ app/serializers.py
@@ -0,0 +1,15 @@
+# app/serializers.py
+from rest_framework import serializers
+from .models import Invoice, InvoiceDetail
+
+class InvoiceDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InvoiceDetail
+        fields = '__all__'
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    details = InvoiceDetailSerializer(many=True, required=False)
+
+    class Meta:
+        model = Invoice
+        fields = '__all__'
---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+# app/views.py
+from rest_framework import viewsets
+from .models import Invoice, InvoiceDetail
+from .serializers import InvoiceSerializer, InvoiceDetailSerializer
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+class InvoiceDetailViewSet(viewsets.ModelViewSet):
+    queryset = InvoiceDetail.objects.all()
+    serializer_class = InvoiceDetailSerializer
